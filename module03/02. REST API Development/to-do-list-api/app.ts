5cc2d809bd435d93e619416480d9d169b2efd688
Yosvaldo <219594820+yosvaldo@users.noreply.github.com>
2026-06-24 18:20:28 +0700

    update Jun 24

--- module03/02. REST API Development/to-do-list-api/app.ts
+++ module03/02. REST API Development/to-do-list-api/app.ts
@@ -2,72 +2,20 @@ import express from "express";
 import type { Application, NextFunction, Request, Response } from "express";
 import { APP_PORT } from "./src/configs/env.config.js";
 import apiRouter from "./src/routes/api.route.js";
-import Yup from "./src/libs/yup.js";
-
-// File app.ts harus ada di root folder sebagai entry point aplikasi
-// app.ts hanya akan mengurus hal-hal yang bersifat global untuk seluruh aplikasi, seperti:
-// - setup server dan middleware yang bersifat global (contoh: body parser, cors, dll)
-// - register router untuk setiap resource endpoint yang kita punya (contoh: todoRouter, userRouter, dll)
-// - atau register router untuk grouping endpoint dengan prefix tertentu (contoh: apiRouter dengan prefix /api)
-// - error handling untuk seluruh aplikasi (contoh: 404 not found, 500 internal server error, dll)
-// Sedangkan untuk hal-hal yang bersifat spesifik untuk tiap resource endpoint, seperti:
-// - definisi schema validasi request body untuk tiap endpoint resource
-// - definisi service function untuk operasi CRUD pada resource tertentu
-// - definisi route handler untuk tiap endpoint resource
-// akan didefinisikan di file terpisah sesuai dengan resource-nya masing-masing
-// Contoh untuk resource "todo", kita bisa buat folder src/resources/todo.resource.ts untuk mendefinisikan route handler-nya,
-// src/services/todo.service.ts untuk mendefinisikan service function-nya,
-// src/validations/todo.schema.ts untuk mendefinisikan schema validasi request body-nya,
-// dan src/models/todo.model.ts untuk mendefinisikan tipe data todo item-nya
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({extended: true}));
 
-// Register router untuk endpoint /api
-// Semua route yang didefinisikan di apiRouter akan memiliki prefix /api,
-// misal endpoint untuk todoRouter yang didefinisikan di apiRouter adalah /todos,
-// maka endpoint lengkapnya akan menjadi /api/todos
 app.use("/api", apiRouter);
 
-// Handle 404 errors untuk endpoint yang tidak ditemukan
-// Tidak pakai path karena ini akan menangani semua request yang tidak match dengan route sebelumnya
-app.use((_req: Request, res: Response) => {
-	res.status(404).send({
-		message: "Endpoint tidak ditemukan",
-	});
-});
-
-// Application level error handling
-app.use(
-	(
-		error: Error | unknown,
-		_req: Request,
-		res: Response,
-		next: NextFunction,
-	) => {
-		// Handle tipe error yang spesifik, misal error validasi request body dari Yup
-		// Hasil error validasi harusnya berstatus 400 Bad Request,
-		// bukan 500 Internal Server Error
-		// Karena error instance bisa datang dari Error class berbeda maka isi error object-nya juga
-		// akan berbeda juga. Kita harus rajin baca dokumentasi library yang kita pakai
-		// untuk mengetahui tipe error apa saja yang mungkin dilemparkan oleh library tersebut
-		// dan mengetahui bentuk isi error object-nya supaya bisa kita handle dengan tepat
-		if (error instanceof Yup.ValidationError) {
-			res.status(400).send({
-				message: "Validasi request body gagal",
-				error: error.errors,
-			});
-			return;
-		}
-
-		res.status(500).send({
-			message: "Terjadi error pada server",
-			error: error instanceof Error ? error.message : error,
-		});
-	},
-);
+app.use((error: Error | unknown, req: Request, res: Response, next: NextFunction) => {
+	res.status(500).send({
+		message: "Error happened on server",
+		error: error instanceof Error ? error.message  
+	})
+})
 
 app.listen(APP_PORT, () => {
 	console.log(`Server is running on port ${APP_PORT}`);
